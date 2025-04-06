ab9673235508fb29141dbf77fa5782abe5ec95b2
Morgan <Fivethousandzzz@outlook.com>
2025-04-06 14:47:51 -0400

    continue adding mixins

---
+++ src/main/java/com/idtech/mixins/MixinItemEntity.java
@@ -0,0 +1,8 @@
+package com.idtech.mixins;
+
+import net.minecraft.world.entity.item.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ItemEntity.class)
+public class MixinItemEntity {
+}
